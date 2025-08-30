a132415125d2ffe4f0b43f31086c75bdf6222eec
phamhuynh161099 <phamhuynh161099@gmail.com>
2025-08-30 13:06:27 +0700

    init pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactStrictMode: true, // Tắt Strict Mode
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: '**', // Cho phép tất cả hostname
+//       },
+//       {
+//         protocol: 'http',
+//         hostname: '**', // Cho phép cả http
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  // disable: process.env.NODE_ENV === 'development',
+  // register: true,
+  // skipWaiting: true,
+})
+
+module.exports = withPWA({
+  // Các config khác của Next.js
   reactStrictMode: true, // Tắt Strict Mode
   images: {
     remotePatterns: [
@@ -15,6 +42,4 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-};
-
-export default nextConfig;
+})
